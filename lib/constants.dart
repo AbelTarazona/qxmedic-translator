97daba2d93f90eb66a6be70ad3f63b1ef6467a08
Abel Tarazona <abel.tarazona@urp.edu.pe>
2026-01-21 09:53:00 -0500

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,4 @@
 class Constants {
   static const String wordRecommendations = 'Recomendaciones:\n1. No tener espacios entre las opciones y el texto.\n2. Las opciones deben empezar con las letras a, b, c, d, etc.\n3. Las opciones deben estar separadas por un punto.\n4. Las respuestas correctas deben estar en mayúsculas.';
   static const String excelRecommendations = 'Recomendaciones:\n1. Quitar cabeceras\n2. Empezar desde A1\n3. No dejar espacio entre filas ni columnas\n4. Mantener la última columna como letra respuesta en mayúscula\n5. Tenerlo en la primera hoja';
-  static const String developers = 'Desarrollado por\n- Dra. Layza Chavez\n- Ing. Abel Tarazona';
 }
